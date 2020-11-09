276271ed584cd7395d50a680c722e366da731d0b
Luca Zavarella <lucazavarella@outlook.com>
2020-11-09 14:20:37 +0000

    Hyper-parameter tuning implemented

--- train.py
+++ train.py
@@ -10,19 +10,6 @@ import pandas as pd
 from azureml.core.run import Run
 from azureml.data.dataset_factory import TabularDatasetFactory
 
-# TODO: Create TabularDataset using TabularDatasetFactory
-# Data is located at:
-# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
-
-ds = ### YOUR CODE HERE ###
-
-x, y = clean_data(ds)
-
-# TODO: Split data into train and test sets.
-
-### YOUR CODE HERE ###a
-
-run = Run.get_context()
 
 def clean_data(data):
     # Dict for cleaning data
@@ -50,6 +37,8 @@ def clean_data(data):
 
     y_df = x_df.pop("y").apply(lambda s: 1 if s == "yes" else 0)
     
+    return x_df, y_df
+
 
 def main():
     # Add arguments to script
@@ -58,7 +47,7 @@ def main():
     parser.add_argument('--C', type=float, default=1.0, help="Inverse of regularization strength. Smaller values cause stronger regularization")
     parser.add_argument('--max_iter', type=int, default=100, help="Maximum number of iterations to converge")
 
-    args = parser.parse_args()
+    args, leftovers = parser.parse_known_args() #parser.parse_args()
 
     run.log("Regularization Strength:", np.float(args.C))
     run.log("Max iterations:", np.int(args.max_iter))
@@ -67,6 +56,42 @@ def main():
 
     accuracy = model.score(x_test, y_test)
     run.log("Accuracy", np.float(accuracy))
+    
+    # Save model as -pkl file to the outputs/ folder to use outside the script
+    OUTPUT_DIR='./outputs'
+    os.makedirs(OUTPUT_DIR, exist_ok=True)
+
+    model_file_name = 'bankmarketing_hyperdrive.pkl'
+    joblib.dump(value=model, filename=os.path.join(OUTPUT_DIR, model_file_name))
+
+# TODO: Create TabularDataset using TabularDatasetFactory
+# Data is located at:
+# "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
+
+# +
+from azureml.core import Dataset
+
+# create a FileDataset from image and label files behind public web urls
+web_path = 'https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv'
+
+ds = Dataset.Tabular.from_delimited_files(path=web_path)
+# -
+
+x, y = clean_data(ds)
+
+# TODO: Split data into train and test sets.
+
+# +
+from sklearn.model_selection import train_test_split
+
+x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=0.2, random_state=223)
+x_train.reset_index(inplace=True, drop=True)
+x_test.reset_index(inplace=True, drop=True)
+y_train.reset_index(inplace=True, drop=True)
+y_test.reset_index(inplace=True, drop=True)
+# -
+
+run = Run.get_context()
 
 if __name__ == '__main__':
     main()
